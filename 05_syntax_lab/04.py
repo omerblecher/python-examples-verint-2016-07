cb0d7579e6fa824a1c646c609db25f7c6ce051f0
omerblecher <omer.blecher@verint.com>
2016-07-05 08:32:01 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -2,16 +2,20 @@
 
 print "Enter some lines and the results will be print backward: \n"
 
-results = ""
+results = []
+
 
 while True:
     userInput = raw_input()
     if len(userInput) == 0:
         break
-    results = userInput + "\n" + results
+    results.append(userInput)
+  
 
 #print all lines backward
 if len(results) > 0:
-    print results
+    for i in range(len(results)):
+        print results[len(results) - i - 1]
+
 else:
     print "Empty input"
