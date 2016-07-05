cec0461c07734398a7947cc03cad384d67f774f7
omerblecher <omer.blecher@verint.com>
2016-07-05 11:02:32 +0300

    Solution for: sol-python-verint.syntax-lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -1,8 +1,30 @@
-""" Write a program that selects a random number
-and asks the user to guess it.
+#Guessing number
 
-After each guess print a hint "too large" or "too small" to the user.
+from random import randint
 
-Bonus: To make things interesting, the program should cheat once in a white
-"""
+number = randint(1, 100)
 
+while True:
+    guess = int(raw_input("Guess a number: "))
+    
+    #check if it's the correct number
+    if number == guess:
+        print "Correct!"
+        break
+
+    firstCompare = guess
+    secondCompare = number
+
+    #get a random number
+    mistake = randint(1, 100)
+
+    #if the number is divided by 19, then we print wrong message
+    if mistake % 19 == 0:
+        firstCompare = number
+        secondCompare = guess
+
+    #Print messages
+    if firstCompare > secondCompare:
+        print "Too big"
+    else:
+        print "Too small"
