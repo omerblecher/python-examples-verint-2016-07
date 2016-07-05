09a4ad48b46762ae1641485c4b9f0e6b850e0852
omerblecher <omer.blecher@verint.com>
2016-07-05 08:47:07 +0300

    Solution for: sol-python-verint.syntax-lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,27 @@
-"""
-Write a program that randomizes 2 numbers
-and calculates their least common multiplier,
-that is the smallest number that is divisable
-by both.
-For example if the numbers were 4 and 6,
-program should print 12
-"""
+#lcm
+from random import randint
+
+firstNumner = randint(1, 10)
+
+secondNumner = randint(1, 10)
+
+gcd = 0
+a = firstNumner
+b = secondNumner
+
+#Calculate gcd
+while True:
+    if(a == b):
+        gcd = a
+        break;
+    elif a > b:
+        a -= b
+    else:
+        b -= a
+
+#Set lcm
+
+lcm = (firstNumner * secondNumner) / gcd
+
+print "The lcm of %d and %d is %d" % (firstNumner, secondNumner,  lcm)
 
