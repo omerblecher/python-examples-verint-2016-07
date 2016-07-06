5340db14a7327272fa222e091092974307a259ac
omerblecher <omer.blecher@verint.com>
2016-07-06 14:06:12 +0300

    Solution for: sol-python-verint.modules-lab/02_bonus.py

--- 06_modules_lab/02_bonus.py
+++ 06_modules_lab/02_bonus.py
@@ -1,10 +1,19 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-Bonus: Print error messages for invalid inputs.
+#sum exercise bonus - check parameters
 
-To print error messages we'll have to use a concept not yet learned in the
-course, and which will only be presented later: Exceptions.
-We'll tap into python's error handling and change its default
-error message to something more meaningful.
-"""
+import sys
 
+
+
+if (len(sys.argv) < 3):
+    print "You need to set 2 integer numbers as parameters to the script"
+elif not(sys.argv[1].isdigit()) and not(sys.argv[1].startswith('-') and sys.argv[1][1:].isdigit()): # Validate that argv[1] is positive or negative integer
+    print "{} is not a valid integer".format(sys.argv[1])
+elif not(sys.argv[2].isdigit()) and not(sys.argv[2].startswith('-') and sys.argv[2][1:].isdigit()): # Validate that argv[2] is positive or negative integer
+    print "{} is not a valid integer".format(sys.argv[2])
+else:
+    first = int(sys.argv[1])
+    second = int(sys.argv[2])
+    
+  
+
+    print "%d + %d = %d" %(first, second, first + second)
