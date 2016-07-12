7b78804cf1850808a7cc3c1323a00cb095c08c7b
omerblecher <omer.blecher@verint.com>
2016-07-12 10:40:56 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -11,8 +11,7 @@ hostnames = set(sys.argv[1:])
 #Read host file and save the host & I.P address in the dictionary
 with open("hosts", "r") as fin:
     for line in fin:
-        hostaddress = line.strip('\n').split('=')
-        (host, address) = hostaddress
+        (host, address) = line.strip('\n').split('=')
         addresses[host] = address
 
 #Go over the given names in the argument line and print the I.P address
