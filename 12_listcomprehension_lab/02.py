bad3d9ba44036e478bbd5d1ec6ac27a11bc22c8a
omerblecher <omer.blecher@verint.com>
2016-07-12 22:13:34 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,10 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
+#All three letters permutation
 
+#Generate a list of all lower letters
+lowerletters = [chr(index) for index in range(1,256) if index >= ord('a') and index <= ord('z')]
+
+#Generate all three letters permutations
+threeletters = [a + b + c for a in lowerletters for b in lowerletters for c in lowerletters]
+
+#Print all permutations
+print " ".join(threeletters)
