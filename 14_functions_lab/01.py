73d3befc226ea47450aeb0c4575cf454b5a51d4f
omerblecher <omer.blecher@verint.com>
2016-07-14 00:19:01 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,21 @@
-"""
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
-"""
+#sum of a list
+def mysum(*nums):
+    return sum([n for n in nums if type(n) == int])
+
+#multiple of a list
+def mymul(*nums):
+    list = [n for n in nums if type(n) == int]
+    list.extend([1,1])            #Add 1,1 in a case when there is no number
+    return reduce(lambda x, y: x*y, list)
+
+#testing
+print mysum(20, 30)
+print mymul(3,4,5,6)
+print mysum(30)
+print mymul(54)
+print mysum(30, "sd", "sd", 5)
+print mymul()
+print mysum("sd", "sd")
+print mymul("sd", "sd")
+
 
