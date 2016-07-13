1a57d402001cd7a95a40b8f8c765791c8eb29dff
omerblecher <omer.blecher@verint.com>
2016-07-13 08:55:14 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,7 +1,7 @@
 #All three letters permutation
 
 #Generate a list of all lower letters
-lowerletters = [chr(index) for index in range(1,256) if index >= ord('a') and index <= ord('z')]
+lowerletters = [chr(index) for index in range(ord('a'),ord('z'))]
 
 #Generate all three letters permutations
 threeletters = [a + b + c for a in lowerletters for b in lowerletters for c in lowerletters]
