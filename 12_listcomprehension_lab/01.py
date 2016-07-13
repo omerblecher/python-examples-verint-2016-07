390bf68d33e14f999205ab35212dff39aff7ab41
omerblecher <omer.blecher@verint.com>
2016-07-13 09:14:10 +0300

    Solution for: sol-python-verint.list-comprehension-lab/01.py

--- 12_listcomprehension_lab/01.py
+++ 12_listcomprehension_lab/01.py
@@ -1,5 +1,5 @@
 #List of all lower case letters
 
 #Generate a list of all lower letters
-lowerletters = [chr(index) for index in range(ord('a'),ord('z'))]
+lowerletters = [chr(index) for index in range(ord('a'),ord('z') + 1)]
 print " ".join(lowerletters)
