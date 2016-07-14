38f713b0aff0356c444c09387c16eb752da0881f
omerblecher <omer.blecher@verint.com>
2016-07-14 15:02:58 +0300

    Solution for: sol-python-verint.functions-lab/05.py

--- 14_functions_lab/05.py
+++ 14_functions_lab/05.py
@@ -1,9 +1,9 @@
 #Create dictionary from a list of arguments
 from collections import defaultdict
 
-def groupby(func, *params):
+def groupby(func, listToGroup):
     groupbydict = defaultdict(list)
-    [groupbydict[func(word)].append(word) for word in params]
+    [groupbydict[func(word)].append(word) for word in listToGroup]
     return groupbydict
 
-print groupby(lambda(s): s[0], 'hello', 'hi', 'help', 'bye', 'here')
+print groupby(lambda(s): s[0], ('hello', 'hi', 'help', 'bye', 'here'))
