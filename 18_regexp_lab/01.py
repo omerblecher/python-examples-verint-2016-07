b43fbedc7a436319f043946fa0a1a97149a46e75
omerblecher <omer.blecher@verint.com>
2016-07-18 13:00:10 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -3,19 +3,19 @@
 import re
 import sys
 
-(defFile, key) = sys.argv[1:]
+(defFile, userkey) = sys.argv[1:]
 defDict = {}
 
 #Insert value into the dictionary
 with open(defFile, "r") as fin:
     for line in fin:
         word = line.strip(' \t\n\r')
-        if not word.startswith('#'):
-            keyvalue = re.split(r'[=]', line)
-            defDict[keyvalue[0].strip(' \t\n\r')] = keyvalue[1].strip(' \t\n\r')
+        if len(word) > 0 and not word.startswith('#'):
+            (key, value) = re.split(r'[=]', line)
+            defDict[key.strip(' \t\n\r')] = value.strip(' \t\n\r')
 
 #Print the value that mact
-if key in defDict.keys():
-    print defDict[key]
+if userkey in defDict.keys():
+    print defDict[userkey]
 else:
-    print "%s is not in the definition file" % (key)
+    print "%s is not in the definition file" % (userkey)
