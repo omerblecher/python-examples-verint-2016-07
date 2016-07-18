b5e2b9e33ad1ef06fb145d654892ae5f7a8fa0fd
omerblecher <omer.blecher@verint.com>
2016-07-18 10:58:54 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -1,12 +1,21 @@
-"""
-Write a program that reads data
-from property files.
-Each line in the file can either be:
-    An empty line
-    A comment line (Start with #)
-    A property line (of the form key = value)
+#Definition file
 
-Write a program that takes a property file name and key
-as command line arguments and prints the requested value
-"""
+import re
+import sys
 
+(defFile, key) = sys.argv[1:]
+defDict = {}
+
+#Insert value into the dictionary
+with open(defFile, "r") as fin:
+    for line in fin:
+        word = line.strip(' \t\n\r')
+        if not word.startswith('#'):
+            keyvalue = re.split(r'[=]', line)
+            defDict[keyvalue[0].strip(' \t\n\r')] = keyvalue[1].strip(' \t\n\r')
+
+#Print the value that mact
+if key in defDict.keys():
+    print defDict[key]
+else:
+    print "%s is not in the definition file" % (key)
