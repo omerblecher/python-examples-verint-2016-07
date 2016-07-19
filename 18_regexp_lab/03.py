4bbb83b897345632018b075b1d9373cd7b781ca1
omerblecher <omer.blecher@verint.com>
2016-07-19 16:19:15 +0300

    Solution for: sol-python-verint.regexp-lab/03.py

--- 18_regexp_lab/03.py
+++ 18_regexp_lab/03.py
@@ -7,6 +7,9 @@ with open(sys.argv[1], "r") as fin:
     for line in fin:
         word = line.strip(' \t\n\r')
         if len(word) > 0 :
-            res = re.search(r'^(.+),(.+),(.+)$', word) #Get all the column values
-            if res is not None:
-                print "%s,%s,%s" % (res.group(2), res.group(1), res.group(3))
+
+            columns = re.findall(r'(.+?)(?:,|$)', word) #Get all the column values
+            temp = columns[0]
+            columns[0] = columns[1]
+            columns[1] = temp #Swap
+            print ','.join(columns)
