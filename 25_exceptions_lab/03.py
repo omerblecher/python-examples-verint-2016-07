49266e320da4ffc5bd178b1632b7564d8e9f6092
omerblecher <omer.blecher@verint.com>
2016-07-25 17:03:15 +0300

    Solution for: sol-python-verint.exceptions-lab/03.py

--- 25_exceptions_lab/03.py
+++ 25_exceptions_lab/03.py
@@ -1,4 +1,32 @@
-"""
-Write code to make the following unit test pass
-"""
+#Unit test that checks image file format
+import unittest
 
+#Define the exception class
+class InvalidImageExt(Exception):
+    def __init__(self, msg):
+        super(InvalidImageExt, self).__init__(msg)
+
+class ImageFile (object):
+    
+    #All image file extensions
+    extensions = {"BMP", "GIF", "IMG", "JBG", "JPE", "JPEG", "JPG", "PBM", "PCD", "PCX", "PCT", "PGM", "PNG", "PPM", "PSD", "TIFF", "TIF"}
+
+    #Constructor
+    def __init__(self, fileName):
+        fileExtension = fileName.split(".")[1]
+        if not fileExtension.upper() in ImageFile.extensions:
+            raise InvalidImageExt("File name %s doesn't end with any image file suffix" % (fileName))
+
+#Unit test class
+class TestImageFile(unittest.TestCase):
+    def test_good_ext(self):
+        try:
+            img = ImageFile("file.png")
+        except InvalidImageExt:
+            self.fail("png should be a valid file extension")
+
+    def test_bad_ext(self):
+        with self.assertRaises(InvalidImageExt):
+            img = ImageFile("file.mp3")
+
+unittest.main()
