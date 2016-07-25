62eb4c122f22d71bd2468b282fb7e380ce352a54
omerblecher <omer.blecher@verint.com>
2016-07-25 13:21:38 +0300

    Solution for: sol-python-verint.exceptions-lab/01.py

--- 25_exceptions_lab/01.py
+++ 25_exceptions_lab/01.py
@@ -1,7 +1,26 @@
-"""
-Write a python program that takes numbers in a loop
-and for each number prints its square root
-If value is negative or not a number show 
-a warning and keep reading values
-"""
+#SQRT - Raise exception if object is not a number
+import math
 
+def sqrt(input):
+
+    #Convert to int. The caller should catch the exception
+    number = int(input)
+    
+    #Check if greater than 0
+    if number < 0:
+        raise ValueError("Expected parameter >= 0, Got: %d" % number)
+    return math.sqrt(number)
+
+#Infinite loop till the user quit
+while True:
+    input = raw_input("Please enter a number, or q to quit: ")
+    
+    #break means the 
+    if input.startswith("q"):
+        print "Bye bye"
+        break
+
+    try:
+        print sqrt(input)
+    except Exception as e:
+        print "Bad input. Details: ", e
